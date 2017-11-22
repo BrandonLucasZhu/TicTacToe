8375a7962d9c950b2b1b4bd73bc65750e1d8b1a9
Brandon Lucas Zhu <Brandonlucaszhu@gmail.com>
2017-11-21 20:49:01 -0500

    Added grid system

--- app/src/main/java/project/basic/brandonzhu/tictactoe/MainActivity.java
+++ app/src/main/java/project/basic/brandonzhu/tictactoe/MainActivity.java
@@ -1,13 +1,18 @@
 package project.basic.brandonzhu.tictactoe;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +22,25 @@ public class MainActivity extends AppCompatActivity {
         //Button numb1 = ((Button)this.findViewById(R.id.b));
         //numb1.setOnClickListener(this);
 
-        Button b = (Button)findViewById(R.id.b);
-        String buttonText = b.getText().toString();
+       // String buttonText = b.getText().toString();
+        Button buttons[][] = new Button[3][3];
 
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // button 1 was clicked!
+        for(int i=0; i<3; i++) {
+            for (int j = 0; j < 3; j++) {
+                String buttonID = "t" + i  + j;
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                buttons[i][j] = ((Button) findViewById(resID));
+                buttons[i][j].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // button 1 was clicked!
+                        Context context = getApplicationContext();
+                        Toast.makeText(context,"Hello",1).show();
+                        // b.setVisibility(View.VISIBLE);
+                    }
+                });
             }
-        });
+        }
     }
 
 
