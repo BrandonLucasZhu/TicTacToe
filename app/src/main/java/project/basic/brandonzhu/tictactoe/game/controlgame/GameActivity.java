e3444395b7df4f52753dcb4dd42ea616b5ddff6a
Brandon Lucas Zhu <Brandonlucaszhu@gmail.com>
2017-12-18 15:53:35 -0500

    Re-modifying application

---
+++ app/src/main/java/project/basic/brandonzhu/tictactoe/game/controlgame/GameActivity.java
@@ -0,0 +1,98 @@
+package project.basic.brandonzhu.tictactoe.game.controlgame;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import project.basic.brandonzhu.tictactoe.game.modelgamelogic.TicTacToe;
+import java.util.ArrayList;
+
+import project.basic.brandonzhu.tictactoe.R;
+
+
+public class GameActivity extends AppCompatActivity {
+
+    private String addr_button;
+    public int count;
+    private TicTacToe tictactoe;
+
+    public void viewBoard(new)
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Button numb1 = ((Button)this.findViewById(R.id.b));
+        //numb1.setOnClickListener(this);
+
+       // String buttonText = b.getText().toString();
+        Button buttons[] = new Button[9];
+
+        for(int i = 0; i < 9; i++) { //Implement magic square
+           // for (int j = 0; j < 3; j++) {
+                String buttonID = "t" + i;
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                buttons[i] = ((Button) findViewById(resID));
+                buttons[i].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Context context = getApplicationContext();
+                        //getResources().getResourceEntryName(v.getId())
+                        addr_button = getResources().getResourceEntryName(v.getId());
+                       // change_button(v.getId(),count,addr_button);
+
+                        //Log.i("hi",getResources().getResourceEntryName(v.getId()));
+                        //Toast.makeText(context,v.getId(),1).show();
+                    }
+                });
+            }
+
+    }
+
+
+
+
+/*
+
+
+    private void reset (ArrayList<Integer> array_buttons) {
+        for (int j = 0; j < array_buttons.size(); j++) {
+            Button reset = (Button) findViewById(array_buttons.get(j));
+            reset.setBackgroundResource(R.drawable.molang_neutral);
+            reset.setClickable(true);
+        }
+    }
+*/
+
+/*
+    private void change_button(int idval, int oddeven, String location){
+
+
+        Button changePic = (Button)findViewById(idval);
+
+        if (oddeven%2== 0){
+            changePic.setBackgroundResource(R.drawable.molang_o);
+            changePic.setClickable(false);
+            save_o.add(Integer.valueOf(location.substring(1)));
+        }
+        else{
+            changePic.setBackgroundResource(R.drawable.molang_x);
+            changePic.setClickable(false);
+            save_x.add(Integer.valueOf(location.substring(1)));
+        }
+
+    }
+
+*/
+
+
+
+
+}
---
+++ app/src/main/java/project/basic/brandonzhu/tictactoe/game/modelgamelogic/TicTacToe.java
@@ -0,0 +1,89 @@
+package project.basic.brandonzhu.tictactoe.game.modelgamelogic;
+
+/**
+ * Created by ezzhubr on 12/18/2017.
+ */
+
+public class TicTacToe {
+
+    private Player[][] board = new Player[3][3];
+    private Player user;
+    private int turnCounter = 0;
+    private String turn;
+
+    private enum Player{
+        X,O,NA
+    }
+
+    private TicTacToe(){
+
+        for (int i = 0; i<board.length; i++){
+            for (int j = 0; j<board.length; j++){
+                board[i][j] = Player.NA;
+            }
+        }
+    }
+
+    public Player getBoardPoint(int i, int j){
+        return board[i][j];
+    }
+
+    public Player setMove(int i, int j, Player user) {
+        board[i][j] = user;
+    }
+
+    public void playerTurn(){
+        turn = (turnCounter%2 == 0)?"X":"O";
+    }
+
+
+    public boolean check_winner(){
+        boolean winner = false;
+        int rowX, rowO, columnX, columnO;
+
+        for (int x = 0; x <board.length; x++){
+        //Check column and row
+            for (int i = 0; i < board.length; i++) {
+                if (board[x][i].equals(Player.X.toString())){
+                    rowX++;
+                }
+                else if(board[x][i].equals(Player.O.toString())){
+                    rowO++;
+                }
+                else if (board[i][x].equals(Player.X.toString())){
+                    columnX++;
+                }
+                else if (board[i][x].equals(Player.O.toString())){
+                    columnO++;
+                }
+            }
+        }
+
+        if (rowX == 3 || columnX == 3 || rowO == 3 || columnO == 3){
+            winner = true;
+        }
+        //Check diagonal
+        if (board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2])){
+            winner = true;
+        }
+        else if (board[2][0].equals(board[1][1]) && board[1][1].equals(board[0][2])){
+            winner = true;
+        }
+
+    return winner;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
